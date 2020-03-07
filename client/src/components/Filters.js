09573315d5fd6817c4c63a4e7072f46f9e60a3a3
Frank Moran <info@francisfrankmoran.com>
2020-03-06 19:26:27 -0500

    ok

---
+++ client/src/components/Filters.js
@@ -0,0 +1,27 @@
+// import React from "react";
+
+// const renderDepartments = (items) => {
+
+//     let depts = [];
+    
+//     items.forEach(item => {
+//         if (!depts.includes(item.department)) {
+//             depts.push(item.department); 
+//         }
+//     });
+
+//     return depts;
+// }
+
+// function Filters(props) {
+//     return(
+//         <div className="row">
+//             <div className="col">
+//                 Filters: {renderDepartments(props.employees)}
+//             </div>
+
+//         </div>
+//     );
+// }
+
+// export default Filters;
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -5,7 +5,8 @@ import employees from "../employees.json";
 class Table extends React.Component {
 
     state = {
-        employees
+        employees: employees,
+        origEmployees: employees
     }
 
     ascFirstName = () => {
@@ -152,24 +153,80 @@ class Table extends React.Component {
           this.setState(employees);
     }
 
+    handleInputChange = event => {
+      const value = event.target.value;
+      const employees = this.state.origEmployees;
+      const matchedEmployees = [];
+      
+      employees.forEach(employee => {
+        const firstName = employee.firstName.toUpperCase();
+        const lastName = employee.lastName.toUpperCase();
+        const valueToMatch = value.toUpperCase();
+        
+        if (firstName.includes(valueToMatch) || lastName.includes(valueToMatch)){
+          matchedEmployees.push(employee);
+        };
+      });
+
+      // console.log(matchedEmployees);
+      this.setState({ 
+        employees: matchedEmployees
+      });
+      // console.log(this.state);
+    }
+
     
 
     render() {
         return (
         <div className="container">
+            {/* <Filters employees={employees} /> */}
+            <div className="row">
+              <div className="col-12 py-3">
+              <input
+                onChange={this.handleInputChange}
+                value={this.value}
+                name="search"
+                type="text"
+                className="form-control"
+                placeholder="Search by name"
+                id="search"
+              />
+                
+              </div>
+            </div>
             <div className="row">
                 <div className="col-12">
                     <table className="w-100">
                         <thead>
                             <tr className="border-bottom">
-                                <th>First Name <span onClick={this.ascFirstName}>ASC</span> <span onClick={this.descFirstName}>DESC</span></th>
-                                <th>Last Name <span onClick={this.ascLastName}>ASC</span> <span onClick={this.descLastName}>DESC</span></th>
-                                <th>Role <span onClick={this.ascRole}>ASC</span> <span onClick={this.descRole}>DESC</span></th>
-                                <th>Department <span onClick={this.ascDepartment}>ASC</span> <span onClick={this.descDepartment}>DESC</span></th>
+                                <th>
+                                  <span className="d-none d-md-inline">First Name</span>
+                                  <span className="d-block d-md-none">First</span>
+                                  <span onClick={this.ascFirstName}><i class="fas fa-arrow-circle-up mx-1 mx-md-2"></i></span> 
+                                  <span onClick={this.descFirstName}><i class="fas fa-arrow-circle-down"></i></span>
+                                </th>
+                                <th>
+                                  <span className="d-none d-md-inline">Last Name</span>
+                                  <span className="d-block d-md-none">Last</span>
+                                  <span onClick={this.ascLastName}><i class="fas fa-arrow-circle-up mx-1 mx-md-2"></i></span> 
+                                  <span onClick={this.descLastName}><i class="fas fa-arrow-circle-down"></i></span>
+                                </th>
+                                <th>
+                                  <span className="d-block d-md-inline">Role</span> 
+                                  <span onClick={this.ascRole}><i class="fas fa-arrow-circle-up mx-1 mx-md-2"></i></span> 
+                                  <span onClick={this.descRole}><i class="fas fa-arrow-circle-down"></i></span>
+                                </th>
+                                <th>
+                                  <span className="d-none d-md-inline">Department</span>
+                                  <span className="d-block d-md-none">Dept.</span>
+                                  <span onClick={this.ascDepartment}><i class="fas fa-arrow-circle-up mx-1 mx-md-2"></i></span> 
+                                  <span onClick={this.descDepartment}><i class="fas fa-arrow-circle-down"></i></span>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <Row employees={employees} />
+                            <Row employees={this.state.employees} />
                         </tbody>
                     </table>
                 </div>
